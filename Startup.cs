695700cecb1ea146a06aaf2361bdc03fe9273339
plamenyovchev <plamenkooo@gmail.com>
2020-02-29 13:58:21 +0200

    Refactoring

--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using AspNetCorePlayground.Middleware;
 using AspNetCorePlayground.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace AspNetCorePlayground
 {
