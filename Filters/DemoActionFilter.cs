555965ff8ff10aa00f701ac5c2d25f33d3efcc88
plamenyovchev <plamenkooo@gmail.com>
2020-02-29 14:57:44 +0200

    Add DemoAsyncActionFilter

---
+++ Filters/DemoActionFilter.cs
@@ -0,0 +1,20 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace AspNetCorePlayground.Filters
+{
+    public class DemoAsyncActionFilter : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            // Do something before the action executes.
+
+            // next() calls the action method.
+            var resultContext = await next();
+
+            // resultContext.Result is set.
+            // Do something after the action executes.
+            // after action executed
+        }
+    }
+}
