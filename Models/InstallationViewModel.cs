13188815ade5f179f72ff4a22596c50ec4ac27e2
plamenyovchev <plamenkooo@gmail.com>
2020-03-02 15:24:57 +0200

    Add InstallationViewModel and implement IValidatableObject

---
+++ Models/InstallationViewModel.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCorePlayground.Models
+{
+    public class InstallationViewModel : IValidatableObject
+    {
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (string.IsNullOrEmpty(Name))
+            {
+                yield return new ValidationResult("Name is required");
+            }
+
+            if (string.IsNullOrEmpty(Address))
+            {
+                yield return new ValidationResult("Address is required");
+            }
+        }
+    }
+}
