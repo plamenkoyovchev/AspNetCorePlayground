8fd3d5ebe8292c319f1f8c449de7f44fbd4860ba
Plamen Yovchev <plamenkooo@gmail.com>
2020-04-10 21:20:50 +0300

    InMemoryChache example

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -1,8 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using AspNetCorePlayground.Data;
+using AspNetCorePlayground.Data.Domain;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace AspNetCorePlayground.Controllers
 {
@@ -11,17 +17,25 @@ namespace AspNetCorePlayground.Controllers
         private readonly PlaygroundContext context;
         private readonly UserManager<IdentityUser> userManager;
         private readonly RoleManager<IdentityRole> roleManager;
+        private readonly IMemoryCache memoryCache;
 
-        public TestController(PlaygroundContext context, UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        public TestController(PlaygroundContext context, UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, IMemoryCache memoryCache)
         {
             this.context = context;
             this.userManager = userManager;
             this.roleManager = roleManager;
+            this.memoryCache = memoryCache;
         }
 
         public IActionResult Get()
         {
-            return Ok(this.context.Installations);
+            if (!this.memoryCache.TryGetValue<IEnumerable<Installation>>("Installations", out var installations))
+            {
+                installations = this.context.Installations.ToList();
+                this.memoryCache.Set("Installations", installations, TimeSpan.FromSeconds(10));
+            }
+
+            return Ok(installations);
         }
 
         public IActionResult Create()
